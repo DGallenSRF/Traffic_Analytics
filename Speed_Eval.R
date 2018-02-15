f8c769dba264b171800561b6ee26a4baa2de5ea5
DGallenSRF <36449104+DGallenSRF@users.noreply.github.com>
2018-02-15 13:37:44 -0600

    added more exploratory stats to Data_C...

--- Speed_Eval.R
+++ Speed_Eval.R
@@ -1,6 +1,7 @@
 library(lubridate)
 library(magrittr)
 library(dplyr)
+library(ggplot2)
 
 setwd("H:/Projects/11000/11187/TS/Task 1")
 
@@ -10,6 +11,7 @@ str(scott_read)
 
 scott <- scott_read
 scott$lubridate <- as.POSIXct(scott$date_time,format = "%m/%d/%Y %H:%M")
+scott$speed <- scott$distance/(scott$travel_time_all/3600)
 
 ## We will try to use the lubridate column to filter for specific time periods
 ## in case of emergency break glass below
@@ -29,18 +31,43 @@ str(TMC_All)
 
 Month1 <- "April"
 Month2 <- "October"
+quantile_prob <- c(0.05,0.15,.5,0.85,0.95)
+quantile_perc <- c("5%","15%","50%","85%","95%")
+quantiles <- data.frame(quantile_prob,quantile_perc)
+
 
 Data_Charactor <- scott %>%
-  group_by(tmc,months=ceiling_date(lubridate,"month"))%>%
+  group_by(tmc,month=ceiling_date(lubridate,"month"))%>%
   ##group_by(tmc,month)%>%
-  summarise(count = n())%>%
-  arrange(desc(count))
+  summarise(count = n(), 
+            speed_std = sd(speed),
+            speed_mean = mean(speed),
+            speed_median = median(speed),
+            dist =  mean(distance))%>%
+  arrange((speed_std))
 Data_Charactor
 
+ggplot(Data_Charactor)+
+  geom_boxplot(aes)
+
+ggplot(scott[1:75000,],aes(x=speed))+
+  geom_histogram(bins = 75)+
+  facet_wrap(~tmc,nrow = 5)
+
 Data_Charactor_merge <- as_tibble(merge(Data_Charactor,TMC_All,by.y = "tmc"))
+
+##unsure of what 1320 is refering to...
 x <- 1320
+
+## it was unclear where count was coming from in "Speed_DB". "Sheet 2" looks to be doing some group bys, but the April and
+## Oct counts don't seem to match
 Data_Charactor_merge$percent <- Data_Charactor_merge$count/x
 
-View(Data_Charactor_merge##[Data_Charactor_merge$county=="Scott",]
-     )
-View(scott)
+View(Data_Charactor_merge) ##[Data_Charactor_merge$county=="Scott",])
+
+
+
+
+
+
+
