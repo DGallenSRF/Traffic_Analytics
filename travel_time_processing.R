4c33362a4c782e3a17b8ac137ea62fbf28ac6960
DGallenSRF <36449104+DGallenSRF@users.noreply.github.com>
2018-02-26 16:48:37 -0600

    fixed factors in travel time minutes

--- travel_time_processing.R
+++ travel_time_processing.R
@@ -6,18 +6,16 @@ library(plotly)
 library(gridExtra)
 library(scales)
 library(stringr)
+library(forecast)
 
 
+setwd("H:/Projects/11000/11187/TS/Task 2")
 
-dat <- read.csv(dir()[3])
+dat <- read.csv(dir()[11],stringsAsFactors = FALSE)
 
-dim(dat[(dat$Timestamp=='N/A'),])
 dim(dat)
 
-dat[is.na(dat$lubridate),]
-
-head(dat)
-
+dat$Travel.Time..minutes. <- as.numeric(dat$Travel.Time..minutes.)
 dat$lubridate <- as.POSIXct(dat$Timestamp,format = "%Y-%m-%d %H:%M:%S")
 dat$hour <- as.factor(hour(dat$lubridate))
 dat$month <- as.factor(month(dat$lubridate))
@@ -26,53 +24,53 @@ dat$weekday <- factor(weekdays(dat$lubridate),levels = c("Monday","Tuesday","Wed
                                                          "Thursday","Friday","Saturday","Sunday"))
 dat$day <- as.factor(day(dat$lubridate))
 
-head(dat$month)
+head(dat)
 
 ##NAs per hour
 dat[!is.na(dat$lubridate),] %>% 
   group_by(hour) %>%
-  summarise(Count = sum(str_count(Travel.Time..minutes., "N/A")),
+  summarise(Count = sum(is.na(Travel.Time..minutes.)),
             Total = n(),
             Percent_NA=Count/Total) %>%
   mutate(positionInCategory = 1:n())%>%
-  print() %>%
-  ggplot(aes(x=hour,y=Percent_NA,fill=hour))+
-  geom_bar(stat="identity")+
-  theme(axis.text.x = element_text(angle=65, vjust=0.6))+
-  scale_color_gradient(low = "#132B43", high = "#56B1F7")
+  ggplot(aes(x=hour,y=Percent_NA))+
+  geom_bar(stat="identity")
 
 
 ##disstribution of NAs per day between 5am and 9pm
 
 dat[!is.na(dat$lubridate)&dat$hour %in% c(5:21),] %>% 
   group_by(weekday,year) %>%
-  summarise(Count = sum(str_count(Travel.Time..minutes., "N/A")),
+  summarise(Count = sum(is.na(Travel.Time..minutes.)),
             Total = n(),
             Percent_NA=Count/Total) %>%
   mutate(positionInCategory = 1:n())%>%
-  print() %>%
-  ggplot(aes(x=weekday,y=Percent_NA,fill=weekday))+
+  ggplot(aes(x=weekday,y=Percent_NA,fill=year))+
   geom_bar(stat="identity")+
   facet_wrap(~year)+
-  theme(axis.text.x = element_text(angle=65, vjust=0.6))+
-  scale_color_gradient(low = "#132B43", high = "#56B1F7")
+  theme(axis.text.x = element_text(angle=65, vjust=0.6),
+        legend.position = 'none')
 
 ##plot of average travel time per day
-dat[dat$Travel.Time..minutes.!='N/A',] %>% 
+Mean_Travel <- dat[!is.na(dat$Travel.Time..minutes.),] %>% 
   group_by(day=(floor_date(lubridate,"day"))) %>%
-  summarise(Time=mean(as.numeric(Travel.Time..minutes.)),
-            Max=max(as.numeric(Travel.Time..minutes.)),
-            Min=min(as.numeric(Travel.Time..minutes.)),
-            Std=sd(as.numeric(Travel.Time..minutes.)),
-            Median=median(as.numeric(Travel.Time..minutes.))) %>%
-  arrange(desc(Time)) %>%
-  ggplot()+
-  geom_line(aes(day,Time))+
-  geom_line(aes(day,Median),color='red')+
+  summarise(Time=mean((Travel.Time..minutes.)),
+            Max=max((Travel.Time..minutes.)),
+            Min=min((Travel.Time..minutes.)),
+            Std=sd((Travel.Time..minutes.)),
+            Median=median((Travel.Time..minutes.))) %>%
+  arrange(day) %>%
+  print()
+
+Mean_Travel$sm <- ma(Mean_Travel$Time,order=14)
+
+ggplot(Mean_Travel)+
+  geom_line(aes(day,sm))+
+  #geom_line(aes(day,Median),color='red')+
   scale_x_datetime(breaks = date_breaks("1 month"))+
   theme(axis.text.x = element_text(angle=65, vjust=0.6))
 
-## plot of totoal NAs per day from 5am to 9pm
+  ## plot of totoal NAs per day from 5am to 9pm
 g <- dat[dat$hour %in% c(5:21),] %>%
   ##dat %>%
   group_by(day=(floor_date(lubridate,"day"))) %>%
@@ -82,12 +80,12 @@ g <- dat[dat$hour %in% c(5:21),] %>%
   mutate(positionInCategory = 1:n())%>%
   arrange((day))
 
-p <- ggplot(g[1000:1750,],aes(day,Percent_NA))+
+p <- ggplot(g,aes(day,Percent_NA))+
   geom_line()+
   geom_point()+
   scale_x_datetime(breaks = date_breaks("1 day"),
                    labels = date_format("%a"))+
   theme(axis.text.x = element_text(angle=65, vjust=0.6))
-ggplotly(p,tooltip = "Percent_NA")
+p
 
 
