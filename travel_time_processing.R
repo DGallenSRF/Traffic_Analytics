38a66ea47657a6e9a7d511fb000dea1c37b73700
DGallenSRF <36449104+DGallenSRF@users.noreply.github.com>
2018-03-05 10:00:37 -0600

    moved code over to markdown document'

--- travel_time_processing.R
+++ travel_time_processing.R
@@ -1,13 +1,14 @@
-library(lubridate)
 library(magrittr)
-library(dplyr)
-library(ggplot2)
-library(plotly)
-library(gridExtra)
 library(scales)
 library(stringr)
 library(forecast)
-
+library(leaflet)
+library(tidyverse)
+library(reshape2)
+library(plotly)
+library(lubridate)
+library(kableExtra)
+options(knitr.table.format = "html") 
 
 
 
@@ -15,30 +16,82 @@ setwd("H:/Projects/11000/11187/TS/Task 2")
 
 TMC_all <- readxl::read_xlsx("TMC_All.xlsx")
 
+summary(TMC_all)
+TMC_all <- TMC_all[!is.na(TMC_all$distance),]
+
+
 list_csv <- list.files(pattern = "*.csv")
 myfiles <- lapply(list_csv,read.csv)
 files <- mapply(cbind,myfiles,'filename' = list_csv,SIMPLIFY = F)
 
-TMC_data <- do.call("rbind",files)
+TMC_data <- rbind_list(files)
 
-dim(TMC_data)
+dim(TMC_data)[1]
 
-TMC_data$Travel.Time..minutes. <- as.numeric(TMC_data$Travel.Time..minutes.)
-TMC_data$lubridate <- as.POSIXct(TMC_data$Timestamp,format = "%Y-%m-%d %H:%M:%S")
-TMC_data$hour <- as.factor(hour(TMC_data$lubridate))
-TMC_data$month <- as.factor(month(TMC_data$lubridate))
-TMC_data$year <- as.factor(year(TMC_data$lubridate))
-TMC_data$weekday <- factor(weekdays(TMC_data$lubridate),levels = c("Monday","Tuesday","Wednesday",
-                                                         "Thursday","Friday","Saturday","Sunday"))
-TMC_data$day <- as.factor(day(TMC_data$lubridate))
+TMC_data %<>% 
+  mutate(Travel.Time..minutes. = as.numeric(Travel.Time..minutes.),
+         lubridate =as.POSIXct(TMC_data$Timestamp,format = "%Y-%m-%d %H:%M:%S"))
+TMC_data %<>% 
+  mutate(hour = as.factor(hour(TMC_data$lubridate)),
+         month = as.factor(month(TMC_data$lubridate)),
+         year = as.factor(year(TMC_data$lubridate)),
+         weekday = factor(weekdays(TMC_data$lubridate),
+                          levels = c("Monday","Tuesday",
+                                     "Wednesday","Thursday",
+                                     "Friday","Saturday","Sunday")),
+         day = as.factor(day(TMC_data$lubridate)),
+         filename_split = filename) 
 
-head(TMC_data)
+TMC_data %<>% 
+  separate(filename_split,into = c('Route','Direction','From','NA','To'),sep='_')
+TMC_data$To <- sub(".csv",'',TMC_data$To)
 
-dat <- TMC_data %>%
-  filter(filename == paste('US-169_Northbound_118P05065_to_118P05066','.csv',sep=))
+TMC_data_merge <- merge(TMC_data,TMC_all[,c('tmc','latitude','longitude')], 
+                        by.x = 'From',by.y='tmc',all.x=TRUE)
+colnames(TMC_data_merge)[which(names(TMC_data_merge) 
+                               %in% c('latitude','longitude'))] <- c('from_latitude','from_longitude')
+
+TMC_data_merge <- merge(TMC_data_merge,TMC_all[,c('tmc','latitude','longitude')], 
+                        by.x = 'To',by.y='tmc',all.x=TRUE)
+colnames(TMC_data_merge)[which(names(TMC_data_merge) 
+                               %in% c('latitude','longitude'))] <- c('to_latitude','to_longitude')
+
+TMC_Unique <- TMC_data_merge[!duplicated(TMC_data_merge$To),c('To','From',
+                                                              "from_latitude","from_longitude",
+                                                              "to_latitude","to_longitude")]
+
+TMC_GPS1 <- melt(TMC_Unique,measure.vars=c('From'),
+                 id.vars=c('from_latitude','from_longitude'),
+                 value.name = 'TMC')
+TMC_GPS2 <- melt(TMC_Unique,measure.vars=c('To'),
+                 id.vars=c('to_latitude','to_longitude'),
+                 value.name = 'TMC')
+
+colnames(TMC_GPS1) <- c('Lat','Long','Variable','TMC')
+colnames(TMC_GPS2) <- c('Lat','Long','Variable','TMC')
+TMC_GPS <- bind_rows(TMC_GPS1,TMC_GPS2)
+TMC_GPS$label <- paste(TMC_GPS$Variable,':',TMC_GPS$TMC,sep = '') 
+
+
+###dataset
+dat <- TMC_data_merge %>%
+  filter(filename == paste('US-169_Southbound_118N04465_to_118N04464','.csv',sep=''))
+
+dat_GPS1 <- melt(dat[1,],measure.vars=c('From'),
+                id.vars=c('from_latitude','from_longitude'),
+                value.name = 'TMC')
+dat_GPS2 <- melt(dat[1,],measure.vars=c('To'),
+                     id.vars=c('to_latitude','to_longitude'),
+                     value.name = 'TMC')
+
+colnames(dat_GPS1) <- c('Lat','Long','Variable','TMC')
+colnames(dat_GPS2) <- c('Lat','Long','Variable','TMC')
+dat_GPS <- bind_rows(dat_GPS1,dat_GPS2)
+dat_GPS$label <- paste(dat_GPS$Variable,':',dat_GPS$TMC,sep = '') 
+
 
 ##NAs per hour
-dat[!is.na(dat$lubridate),] %>% 
+d <- dat[!is.na(dat$lubridate),] %>% 
   group_by(hour) %>%
   summarise(Count = sum(is.na(Travel.Time..minutes.)),
             Total = n(),
@@ -46,7 +99,7 @@ dat[!is.na(dat$lubridate),] %>%
   mutate(positionInCategory = 1:n())%>%
   ggplot(aes(x=hour,y=Percent_NA))+
   geom_bar(stat="identity")
-
+ggplotly(d)
 
 ##disstribution of NAs per day between 5am and 9pm
 
@@ -71,34 +124,32 @@ Mean_Travel <- dat[!is.na(dat$Travel.Time..minutes.),] %>%
             Std=sd((Travel.Time..minutes.)),
             Median=median((Travel.Time..minutes.))) %>%
   arrange(day)
-
-##x <- dat[dat$year == 2014 &
-           dat$month==9 &
-           dat$day == 15 &
-           !is.na(dat$Travel.Time..minutes.),2]
-##hist(x,breaks=40)
+Mean_Travel$sm <- ma(Mean_Travel$Mean,order=365)
 
 
-Mean_Travel$sm <- ma(Mean_Travel$Mean,order=7)=
-Mean_Travel
+
+colnames(Mean_Travel) %in% colnames(Mean_Travel[,2:7])
 
 ##plot(Mean_Travel$diff)
 
 f <- ggplot(Mean_Travel)+
   geom_line(aes(day,sm),color='red')+
-  #geom_line(aes(day,Time),linetype="dashed",size=.5)+
+  geom_line(aes(day,Mean),linetype="dashed",size=.5)+
   #geom_line(aes(day,Median),color='red')+
   scale_x_datetime(breaks = date_breaks("1 month"))+
   theme(axis.text.x = element_text(angle=65, vjust=0.6))
 ggplotly(f)
 
-ts_Time = ts(Mean_Travel$Mean, frequency = 365,start = c(2014,1))
+ts_Time = ts(Mean_Travel$Mean, frequency = 365,start = c(2014))
 decompose_Time = decompose(ts_Time, "additive")
 
 #plot(as.ts(decompose_Time$seasonal))
 #plot(as.ts(decompose_Time$trend))
 #plot(as.ts(decompose_Time$random))
-my_plot.decomposed.ts(decompose_Time,"Analysis of Mean Travel Time per Day")
+par(family = 'sans',font=10,las=3,pty='m')
+my_plot.decomposed.ts(decompose_Time)
+title(main = "US-169_Southbound_118N04465_to_118N04464\n Mean Travel Time Series Analysis")
+
 
 Mean_Travel$seasonal <- as.numeric((decompose_Time$seasonal))
 
@@ -115,5 +166,15 @@ my_plot.decomposed.ts = function(x, title="", ...) {
        main=title, ...)
 }
 
+head(dat)
 
+m <- leaflet(data=dat_GPS) %>% 
+  setView(lng = -93.554508, lat =  44.766168, zoom = 11)
+
+Traffic_camera <- makeIcon('Traffic_camera.png',iconWidth = 24, iconHeight = 20)
+
+m %>% 
+  addTiles() %>% 
+  addMarkers(~Long,~Lat,icon=Traffic_camera,label = ~label) %>%
+  addProviderTiles(providers$OpenStreetMap)
 
